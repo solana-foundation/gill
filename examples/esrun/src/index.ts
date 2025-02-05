97f786066bd9f7be4bd62016a489a68c43d068ab
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-02-05 11:07:09 -0500

    feat: add examples

---
+++ examples/esrun/src/index.ts
@@ -0,0 +1,89 @@
+import {
+  createSolanaClient,
+  createSolanaRpc,
+  LAMPORTS_PER_SOL,
+  mainnet,
+  sendAndConfirmTransactionFactory,
+} from "gill";
+import { loadKeypairSignerFromFile } from "gill/node";
+
+const signer = await loadKeypairSignerFromFile();
+
+console.log("address:", signer.address);
+console.log("LAMPORTS_PER_SOL:", LAMPORTS_PER_SOL);
+
+// @ts-ignore
+const rpc2 = createSolanaRpc("derp");
+// @ts-ignore
+const rpc3 = createSolanaRpc(mainnet("d"));
+
+const { rpc, rpcSubscriptions } = createSolanaClient({
+  // urlOrMoniker: "devnet",
+  // urlOrMoniker: "d",
+  urlOrMoniker: "mainnet",
+  // urlOrMoniker: "",
+});
+
+// @ts-ignore
+const { rpc: mainnetRpc } = createSolanaClient({
+  urlOrMoniker: "mainnet",
+});
+// @ts-ignore
+const { rpc: devnetRpc } = createSolanaClient({
+  urlOrMoniker: "devnet",
+});
+
+const { value: latestBlockhash } = await rpc.getLatestBlockhash().send();
+console.log("latestBlockhash:", latestBlockhash);
+
+// @ts-ignore
+const sendAndConfirmTransaction = sendAndConfirmTransactionFactory({
+  rpc,
+  rpcSubscriptions,
+});
+
+// let tx = createTransaction({
+//   feePayer: signer,
+//   instructions: [],
+//   version: 0,
+//   latestBlockhash,
+// });
+
+// console.log(tx);
+
+// let signedTransaction = await signTransactionMessageWithSigners(tx);
+// signedTransaction = await signTransactionMessageWithSigners(
+//   setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, tx),
+// );
+// tx = setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, tx);
+// signedTransaction = await signTransactionMessageWithSigners(tx);
+
+// console.log("signedTransaction");
+// console.log(signedTransaction);
+
+// let signature = getSignatureFromTransaction(signedTransaction);
+// console.log("signature:", signature);
+
+// try {
+//   await sendAndConfirmTransaction(signedTransaction, {
+//     commitment: "confirmed",
+//   });
+
+//   console.log(`https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+// } catch (e) {
+//   // console.error("Unable to send and confirm");
+//   // console.error(err);
+
+//   if (isSolanaError(e, SOLANA_ERROR__JSON_RPC__SERVER_ERROR_SEND_TRANSACTION_PREFLIGHT_FAILURE)) {
+//     const preflightErrorContext = e.context;
+//     const preflightErrorMessage = e.message;
+//     const errorDetailMessage = isSystemError(e.cause, transaction)
+//       ? getSystemErrorMessage(e.cause.context.code)
+//       : e.cause?.message;
+//     console.error(preflightErrorContext, " - ", preflightErrorMessage);
+//     console.log();
+//     console.log(errorDetailMessage);
+//   } else {
+//     throw e;
+//   }
+// }
