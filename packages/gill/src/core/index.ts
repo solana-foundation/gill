b8a5c4e4fba8502ac52e8e404209c72d4fd95616
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-02-11 16:19:15 -0500

    feat: sending help

--- packages/gill/src/core/index.ts
+++ packages/gill/src/core/index.ts
@@ -4,4 +4,6 @@ export * from "./rpc";
 export * from "./explorer";
 export * from "./transactions";
 export * from "./base64-transactions";
+export * from "./send-and-confirm-transaction";
+export * from "./prepare-transaction";
 export * from "./accounts";
