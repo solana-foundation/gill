aa455f24853e599353ddb18d30ab2d8336d64350
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-02-12 09:48:58 -0500

    fix: removed import

--- packages/gill/src/core/index.ts
+++ packages/gill/src/core/index.ts
@@ -4,6 +4,5 @@ export * from "./rpc";
 export * from "./explorer";
 export * from "./transactions";
 export * from "./base64-transactions";
-export * from "./send-and-confirm-transaction";
 export * from "./prepare-transaction";
 export * from "./accounts";
