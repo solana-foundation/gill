3c1b2957d7f067a7cca5220a6b22ebbd18eb45d7
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-02-27 11:17:19 -0500

    refactor: correct decoder

--- packages/gill/src/core/base64-from-transaction.ts
+++ packages/gill/src/core/base64-from-transaction.ts
@@ -1,9 +1,5 @@
 import { getBase64Encoder } from "@solana/codecs";
-import {
-  getTransactionDecoder,
-  type SignaturesMap,
-  type TransactionMessageBytes,
-} from "@solana/transactions";
+import { getTransactionDecoder, type Transaction } from "@solana/transactions";
 import type { transactionToBase64, transactionToBase64WithSigners } from "./base64-to-transaction";
 
 /**
@@ -11,9 +7,6 @@ import type { transactionToBase64, transactionToBase64WithSigners } from "./base
  *
  * Use {@link transactionToBase64} or {@link transactionToBase64WithSigners} to create the base64 encoded transaction string
  */
-export function transactionFromBase64(base64EncodedTransaction: string): {
-  messageBytes: TransactionMessageBytes;
-  signatures: SignaturesMap;
-} {
+export function transactionFromBase64(base64EncodedTransaction: string): Transaction {
   return getTransactionDecoder().decode(getBase64Encoder().encode(base64EncodedTransaction));
 }
