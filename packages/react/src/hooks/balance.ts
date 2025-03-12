c48f4d4b192d2de8a29e509b1f9ea22216cb0c53
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-03-12 14:20:03 -0400

    fix: hook in body

--- packages/react/src/hooks/balance.ts
+++ packages/react/src/hooks/balance.ts
@@ -6,15 +6,15 @@ import { useSolanaClient } from "./client";
 import { GILL_HOOK_CLIENT_KEY } from "../const";
 
 /**
- * Get an account's balance the using the Solana RPC method [`getBalance`](https://solana.com/docs/rpc/http/getbalance)
+ * Get an account's balance the using the Solana RPC method
+ * of [`getBalance`](https://solana.com/docs/rpc/http/getbalance)
  */
 export function useBalance(address: string | Address) {
+  const { rpc } = useSolanaClient();
   const { data: balance, ...rest } = useQuery({
     queryKey: [GILL_HOOK_CLIENT_KEY, "balance", address],
     queryFn: async () => {
-      const { value: balance } = await useSolanaClient()
-        .rpc.getBalance(address as Address)
-        .send();
+      const { value: balance } = await rpc.getBalance(address as Address).send();
       return balance;
     },
     networkMode: "offlineFirst",
