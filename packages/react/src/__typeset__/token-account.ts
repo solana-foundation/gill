afa0ec7cefd4850bed7f7f017ebd77cdfac80389
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-05-01 13:03:25 -0400

    test: fixed tests

--- packages/react/src/__typeset__/token-account.ts
+++ packages/react/src/__typeset__/token-account.ts
@@ -1,20 +1,21 @@
 import { Account, Address } from "gill";
 
 import { useTokenAccount } from "../hooks/token-account";
+import { Token } from "gill/programs/token";
 
 // [DESCRIBE] useTokenAccount
 {
-  const mint = null as unknown as Address<"12345">;
-  const owner = null as unknown as Address<"12345">;
-  const ata = null as unknown as Address<"12345">;
+  const mint = null as unknown as Address<"mint">;
+  const owner = null as unknown as Address<"owner">;
+  const ata = null as unknown as Address<"ata">;
 
-  // Should accept mint and owner
+  // Should accept `mint` and `owner`
   {
     const { account } = useTokenAccount({ mint, owner });
     // Should have `exists=true` declared
     account satisfies { exists: true };
-    // Should be a Uint8Array for the data
-    account satisfies Account<Uint8Array>;
+    // Should be a Token for the data
+    account satisfies Account<Token>;
 
     // @ts-expect-error - Should not allow no argument
     useTokenAccount();
@@ -23,12 +24,39 @@ import { useTokenAccount } from "../hooks/token-account";
     useTokenAccount({});
   }
 
-  // Should accept `ata` input`
+  // Should accept `ata` input
   {
     const { account } = useTokenAccount({ ata });
     // Should have `exists=true` declared
     account satisfies { exists: true };
-    // Should be a Uint8Array for the data
-    account satisfies Account<Uint8Array>;
+    // Should be a Token for the data
+    account satisfies Account<Token>;
+    // Should use the address type
+    account.address satisfies Address<"ata">;
+  }
+
+  // Should accept `config` input with `ata`
+  {
+    const { account } = useTokenAccount({
+      ata,
+      config: { commitment: "confirmed" },
+    });
+    // Should have `exists=true` declared
+    account satisfies { exists: true };
+    // Should be a parsed `Token` for the data
+    account satisfies Account<Token>;
+  }
+
+  // Should accept `config` input with `mint` and `owner`
+  {
+    const { account } = useTokenAccount({
+      mint,
+      owner,
+      config: { commitment: "confirmed" },
+    });
+    // Should have `exists=true` declared
+    account satisfies { exists: true };
+    // Should be a parsed `Token` for the data
+    account satisfies Account<Token>;
   }
 }
